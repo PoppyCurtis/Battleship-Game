c2b6d3f7efcd1505040113bc0f6d06aa55123dbd
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-20 14:40:17 +0000

    set and test hit, miss and message icons using  javascript model object

--- battleshipgame.js
+++ battleshipgame.js
@@ -7,31 +7,54 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk == false) {
-    guess = prompt("Ready, aim, fire (enter a number between 0 -6):");
-    
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number!");
-    }
+/*while (isSunk == false) {
+  guess = prompt("Ready, aim, fire (enter a number between 0 -6):");
 
-    else {
-        guesses = guesses + 1;
-    
-      if (guess == location1 || guess == location2 || guess == location3) {
-        alert("HIT!");
-        hits = hits + 1;
-   
-        if (hits == 3) {
-          isSunk = true;
-          alert("You sunk my battleship!");
-        } 
-      }
-      else {
-          alert("MISS!");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+  }
+
+  else {
+    guesses = guesses + 1;
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT!");
+      hits = hits + 1;
+
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sunk my battleship!");
       }
     }
+    else {
+      alert("MISS!");
+    }
+  }
 }
 var stats = "You took " + guesses + " guesses to sink the battleship!"
-+ ", which means your shooting accuracy was " + (3/guesses);
+  + ", which means your shooting accuracy was " + (3 / guesses);
 alert(stats);
+*/
+var view = {
+  displayMessage: function (msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+}
+view.displayMessage("Tap tap, is this thing on?");
+view.displayMiss("00"); 
+view.displayHit("34"); 
+view.displayMiss("45"); 
+view.displayHit("12"); 
+view.displayMiss("25"); 
+view.displayHit("26");
+
 
